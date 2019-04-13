ee2865b78b45fc58b28520f3aef7e7839e752d8a
bencyn <bensonnjung39@gmail.com>
2019-04-13 14:21:42 +0300

    Initial express server with route file

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    // "mongodb+srv://bencyn:ben742285@cluster0-9fsjc.mongodb.net/test?retryWrites=true"
+    "mongodb://bencyn:ben742285@ds135726.mlab.com:35726/heroku_1fxl3wfv"
+};
