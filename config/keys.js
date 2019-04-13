88a831e8b3f47b1b2b965a6fa0ae68dcab5b4a9a
bencyn <bensonnjung39@gmail.com>
2019-04-13 18:01:59 +0300

    - added jwt and passport authentication - register user and login as well validate - how to use protected routes with jwt strategy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI:
     // "mongodb+srv://bencyn:ben742285@cluster0-9fsjc.mongodb.net/test?retryWrites=true"
-    "mongodb://bencyn:ben742285@ds135726.mlab.com:35726/heroku_1fxl3wfv"
+    "mongodb://bencyn:ben742285@ds135726.mlab.com:35726/heroku_1fxl3wfv",
+  secretOrkey: "skadjkl23490sdj*#@)(ksjdk"
 };
